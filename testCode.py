2b3439e93ddcc65942bfac98a33af1af67962c84
OmkarSolanke-23 <omkarsolanke4557@gmail.com>
2026-09-10 17:00:14 +0530

    Add files via upload

---
+++ testCode.py
@@ -0,0 +1,156 @@
+
+# # n = 0
+# # for i in range(0, -3, -1):  
+# #    print(i)
+
+# # result = [] 
+# # n =0
+# # for i in range(3):n    
+# # result.append(i)
+# # n
+# # print(result)
+
+# # for ch in 'py':
+# #     print(ch)
+
+
+# # n = 0
+# # for i in range(3):
+# #     if i== 1:
+# #         break
+# #     print(i)
+
+# # total = 0
+# # for n in [29, 3]:
+# #     total += n
+# # print(total)
+
+# # total = 0
+# # for n in [9, 3]:
+# #     total += n
+# # print(total)
+
+# # for i in range(2, 6, 2):
+# #     print(i)
+
+# # total = 0
+# # for n in [1, 2, 3]:
+# #     total += n
+# # print(total)
+
+# # for i in range(1):
+# #     print(i)
+
+# # for i, v in enumerate(['x','y']):
+# #     print(i)
+
+# #     for i, v in enumerate(['a','b']):
+# #       print(i, v , end=' ')
+
+# # for i in range(3):
+# #        print(i)  
+# #   
+# # for i in range(2, 6, 2):
+# #     print(i)
+
+# # for x in [10]:
+# #     print(x)
+# # else:
+# #     print('done')
+
+# # for i, v in enumerate(['x','y']):
+# #     print(i)
+
+# # x = 25
+# # while x < 4:
+# #     print('run')
+# # print('done')
+
+# # i = 2
+# # while i > 0:
+# #     print(i)
+# #     i -= 1
+
+# # i = 1
+# # while i < 4:
+# #     i *= 2
+# # print(i)
+
+# # x = 20
+# # while x < 4:
+# #     print('run')
+# # print('done')
+
+# # i = 0
+# # while i < 3:
+# #     print(i)
+# #     i += 1
+# # x = 5
+# # while x < 4:
+# #     print('run')
+# # print('done')
+
+# # x = 5
+# # while x < 3:
+# #     print('A')
+# # print('B')
+
+# # i = 0
+# # while i < 1:
+# #     print(i)
+# #     i += 1
+
+# # i = 0
+# # while i < 1:
+# #     i += 1
+# # print(i)
+# # i = 3
+# # while i > 0:
+# #     print(i)
+# #     i -= 1
+
+# # for i in range(2):
+# #     pass
+# # print('done')
+
+# # for i in range(3):
+# #     if i == 0:
+# #         break
+# # else:
+# #     print('done')
+# # print('end')
+
+# # for i in range(3):
+# #     if i == 1:
+# #         continue
+# #     print(i)
+
+# # for i  in range(5):
+# #     if i ==1:
+# #         break
+# # print(i)
+
+# for i in range(3):
+#     if i == 5:
+#         break
+# else:
+#     print('finished')
+
+# s = 'Hi'
+# print(s * 3)
+
+# print("a ,b ,c".split(","))
+
+# s =" hello"
+
+# print(s.replace("l" ,"r"))
+
+# print(",".join(["x", "y", "z"]))
+
+s = "python"
+
+print(s.endswith("on"))
+
+s = "Hello World" 
+print(s.replace("World", "Python"))
+
